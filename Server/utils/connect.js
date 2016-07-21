c2130b864a4aa665780ca1e9d015ae36fdd437b9
Thu Nguyen <thu.nguyen@smartdev.vn>
2016-07-21 13:25:32 -0700

    add url mongodb live on site modulusmongo

--- Server/utils/connect.js
+++ Server/utils/connect.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import logger from './logger.js';
 
-var connectionUrl = 'mongodb://localhost/smartcorp';
+// var connectionUrl = 'mongodb://localhost/smartcorp';
+// this is my mongodb live on site modulusmongo use instead no need install mongo on your localhost
+var connectionUrl = 'mongodb://anhthu:123456@apollo.modulusmongo.net:27017/Epiti6qe';
 
 function connection() {
 	mongoose.connect(connectionUrl, function(err) {
